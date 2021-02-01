b43ba1516c7a0a73e07a37b0fc6817572532b750
bharathi-2806 <bharathi.kosaraju04@gmail.com>
2021-02-01 21:54:28 +0530

    Updated code in test3

--- src/test/java/com/test/testcode/TestThree.java
+++ src/test/java/com/test/testcode/TestThree.java
@@ -1,5 +1,12 @@
 package com.test.testcode;
 
+import org.testng.annotations.Test;
+
 public class TestThree {
 
+	@Test
+	public void three() {
+System.out.println("Test three class");
+System.out.println("hello");
+	}
 }
